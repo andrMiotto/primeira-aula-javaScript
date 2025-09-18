08325b074577027d28b451205283ea377dedc0da
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2025-09-18 20:06:42 -0300

    feat:termina

--- Lista2/ex27.js
+++ Lista2/ex27.js
@@ -1,21 +1,26 @@
 /*27 - Crie uma função chamada "criarMatriz" que recebe o número de linhas e o número de colunas
 como parâmetros e retorna uma matriz com essas dimensões preenchida com números aleatórios.*/
 
-
-const prompt = require("prompt-sync")();
-
+const prompt = require('prompt-sync')();
 
 function criarMatriz(linhas, colunas) {
-    const matriz = [];
+    let matriz = [];
+
     for (let i = 0; i < linhas; i++) {
-        const linha = [];
+        let linha = [];
         for (let j = 0; j < colunas; j++) {
             linha.push(Math.floor(Math.random() * 100)); 
         }
         matriz.push(linha);
     }
+
     return matriz;
 }
 
-const minhaMatriz = criarMatriz(3, 4);
-console.log(minhaMatriz);
+let linhas = parseInt(prompt("Digite o número de linhas: "));
+let colunas = parseInt(prompt("Digite o número de colunas: "));
+
+let matriz2 = criarMatriz(linhas, colunas);
+
+console.log("Matriz gerada:");
+console.table(matriz2);
